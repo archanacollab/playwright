061f6bfbab77cd0e0376ce97b500f50f70368b29
Archana <archanasivankutty92@gmail.com>
2025-08-17 17:03:40 +0530

    added actions

--- Utils/jsonReader.js
+++ Utils/jsonReader.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 function readJSON(relativePathFromParent) {
-  const repoRoot = process.cwd();
-    const fullPath = path.join(repoRoot, relativePathFromParent); 
-    if (!fs.existsSync(fullPath)) {
-        throw new Error(`File not found: ${fullPath}`);
+  //const repoRoot = process.cwd();
+    //const fullPath = path.join(repoRoot, relativePathFromParent); 
+    if (!fs.existsSync(relativePathFromParent)) {
+        throw new Error(`File not found: ${relativePathFromParent}`);
     }
-    const rawData = fs.readFileSync(fullPath, 'utf-8');
+    const rawData = fs.readFileSync(relativePathFromParent, 'utf-8');
     return JSON.parse(rawData);
 }
 module.exports = { readJSON };
